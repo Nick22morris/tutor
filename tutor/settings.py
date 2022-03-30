301666414311fdbe7a19ae45ed9539b2d2efe39d
Nick22morris <nick88morris22@gmail.com>
2022-03-30 10:45:09 -0500

    setup for deployment

--- tutor/settings.py
+++ tutor/settings.py
@@ -23,15 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xo6$1-#5jg(&jig)v-n!__rg8&3vsg-=c!f$a@56*#(^^b(3-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
