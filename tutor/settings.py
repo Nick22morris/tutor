0f940520f407348247437888ddce6c77468dc1dc
Nick22morris <nick88morris22@gmail.com>
2022-04-02 13:11:52 -0500

    bug patch

--- tutor/settings.py
+++ tutor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xo6$1-#5jg(&jig)v-n!__rg8&3vsg-=c!f$a@56*#(^^b(3-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000/',
                  'tutor-dryden.herokuapp.com/']
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
